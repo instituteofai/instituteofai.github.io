040d09dbe2234d3da7f2c77005d49733d101ea56
I322118 <ravi.suresh.mashru@sap.com>
2019-11-13 19:07:36 +0530

    Add units for Machine Learning

--- src/pages/machine-learning.js
+++ src/pages/machine-learning.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import mlJson from './machine-learning.json';
 
 const BlogPage = () => (
     <Layout>
         <SEO title="Machine Learning" />
-        <div>Get an introduction to Machine Learning.</div>
-        <div>Learn Python and the major libraries used for Machine Learning with Python.</div>
+        <h1>Machine Learning</h1>
+        <div>Get an introduction to Machine Learning, with a strong focus on practically applying what you learn.</div>
+        <div className="cu-cont">
+            {mlJson.units.map((unit, idx) => {
+            return <div className="cu-unit">
+                <div className="">
+                    <span className="cu-num">{idx+1}</span><span className="cu-title">{unit.title}</span>
+                </div>
+                <div className="cu-body">
+                    {unit.items && unit.items.map(item => {
+                        return <div>{item}</div>;
+                    })}
+                </div>
+            </div>
+            })}
+        </div>
     </Layout>
 );
 
